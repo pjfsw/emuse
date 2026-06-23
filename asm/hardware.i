3667ec38d1afa35d661a0d7b4313abca8483b5b7
Johan Fransson <per.johan.fransson@gmail.com>
2026-06-23 21:55:21 +0200

    Timer Interrupt working!

--- asm/hardware.i
+++ asm/hardware.i
@@ -1,3 +1,7 @@
+IRQV_TIMER  equ $00006c
+IRQV_BLANK  equ $000070
+IRQV_UART   equ $000074
+
 EXEC_BASE   equ $4
 RAM_LIMIT   equ $800000
 OREG        equ $d00000
@@ -6,12 +10,18 @@ OVR_REG     equ (OREG+OVR)
 OVR_ON      equ 0
 OVR_OFF     equ $2
 
+TIMER_ACT   equ 5
+TIMER_FREQ  equ 7
+TIMER_75HZ  equ 1
+TIMER_150HZ equ 2
+TIMER_300HZ equ 3
 SPI_CS      equ 13
 SPI_MOSI_CLK equ 15
 SPI_CS_OFF  equ 0
 SPI_CS_MMC  equ 1
 SPI_CS_EXT  equ 2
 SPI_CS_ROM  equ 3
+TIMERACK    equ $a00001
 IREG        equ $e00001
 
 SYSTEM_BSS_BASE equ $000400 
--- asm/utils/blinker.asm
+++ asm/utils/blinker.asm
@@ -8,22 +8,58 @@ loop\@:
     bne.s loop\@
     endm
 
+
 START equ $10000
     org START
-    move.l $4,a6
-    lea OREG+SPI_CS,a5
-    moveq #0,d2
-loop:
-    addq.w #1,d2
-    move.w d2,d0
-    jsr -34(a6)  ; conputhex16
-    lea Msg(pc),a1
-    jsr -22(a6)  ; conputs
-    move.b #3,(a5)
-    Delay
-    move.b #0,(a5)
-    Delay
-    bra loop
-Msg:
-    dc.b "!",13,10,0
-    even
+    move.w  #$2700,sr          ; disable while configuring    
+    lea DummyISR,a0
+    move.l a0,IRQV_BLANK
+    move.l a0,IRQV_UART
+    lea TimerISR,a0
+    move.l a0,IRQV_TIMER
+    move.b #TIMER_150HZ,OREG+TIMER_FREQ ; 150 Hz
+    move.b #0,OREG+TIMER_ACT  ; Disable timer
+    move.b #1,OREG+TIMER_ACT  ; Enable timer
+    move.w  #$2200,sr        ; mask level 2, accepts 3–7    
+    bra *
+
+TimerISR:
+    movem.l d0-d1/a0-a1,-(sp)
+    lea BlinkCount,a0
+    move.b (a0),d0
+    subq.b #1,d0
+    move.b d0,(a0)
+    bne .1
+    move.b #75,(a0)
+    move.b BlinkState,d0    
+    eor.b #3,d0
+    move.b d0,BlinkState
+    move.b d0,OREG+SPI_CS
+.1:
+    tst.b TIMERACK      ; Acknowledge interrupt
+    movem.l (sp)+,d0-d1/a0-a1
+    rte
+
+DummyISR:
+    rte    
+
+;    move.l $4,a6
+    ;lea OREG+SPI_CS,a5
+    ;moveq #0,d2
+;loop:
+    ;addq.w #1,d2
+    ;move.w d2,d0
+    ;jsr -34(a6)  ; conputhex16
+    ;move.b #3,(a5)
+    ;Delay
+    ;move.b #0,(a5)
+    ;Delay
+    ;bra loop
+;Msg:
+    ;dc.b "!",13,10,0
+    ;even
+
+BlinkState:
+    dc.b 0
+BlinkCount:
+    dc.b 75
