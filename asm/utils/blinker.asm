16a6a0fa4471b0b1335c9102bf5a4a0058b22ddc
Johan Fransson <per.johan.fransson@gmail.com>
2026-07-07 19:31:42 +0200

    Updated blinker to restore and return

--- asm/utils/blinker.asm
+++ asm/utils/blinker.asm
@@ -8,21 +8,40 @@ loop\@:
     bne.s loop\@
     endm
 
+GETC equ -52
+
 
 START equ $10000
     org START
-    move.w  #$2700,sr          ; disable while configuring    
+    move.w sr,OldSR
+    move.w #$2700,sr          ; disable while configuring    
     lea DummyISR,a0
     move.l a0,IRQV_BLANK
-    move.l a0,IRQV_UART
     lea TimerISR,a0
     move.l a0,IRQV_TIMER
     move.b #TIMER_150HZ,OREG+TIMER_FREQ ; 150 Hz
     move.b #0,OREG+TIMER_ACT  ; Disable timer
     move.b #1,OREG+TIMER_ACT  ; Enable timer
     move.w  #$2200,sr        ; mask level 2, accepts 3–7    
-    bra *
 
+    move.l $4.w,a6
+.flushChars:
+    jsr GETC(a6)
+    bpl.s .flushChars
+
+.waitForChar:    
+    jsr GETC(a6)
+    tst.l d0
+    bmi.s .waitForChar    
+
+    move.w #$2700,sr          ; disable while configuring    
+    move.b #0,OREG+TIMER_ACT  ; disable timer
+    tst.b TIMERACK            ; Acknowledge interrupt    
+    move.w OldSR,sr           ; restore interrupt
+    moveq #0,d0
+    rts
+
+    
 TimerISR:
     movem.l d0-d1/a0-a1,-(sp)
     lea BlinkCount,a0
@@ -43,22 +62,8 @@ TimerISR:
 DummyISR:
     rte    
 
-;    move.l $4,a6
-    ;lea OREG+SPI_CS,a5
-    ;moveq #0,d2
-;loop:
-    ;addq.w #1,d2
-    ;move.w d2,d0
-    ;jsr -34(a6)  ; conputhex16
-    ;move.b #3,(a5)
-    ;Delay
-    ;move.b #0,(a5)
-    ;Delay
-    ;bra loop
-;Msg:
-    ;dc.b "!",13,10,0
-    ;even
-
+OldSR:
+    dc.w 0
 BlinkState:
     dc.b 0
 BlinkCount:
