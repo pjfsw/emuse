6a9d665d8caafa5d8133695d9930a35af2c4f244
Johan Fransson <per.johan.fransson@gmail.com>
2026-08-09 17:43:21 +0200

    Editor stub

--- edit/src/edit.c
+++ edit/src/edit.c
@@ -2,14 +2,38 @@
 
 int main() {
     conclr();
-    conputc('!');
+    consetcrs(25,1);
     conreverse();
-    consetcrs(20,10);
-    conputs("Hello world from C!\r\n");    
+    for (int i = 0; i < 80; i++) {
+        conputc(' ');
+    }
+    concrsleft(80);
+    conputs("/SOMEFILE.TXT");
+    consetcrs(25,70);
+    conputs("1/???");    
     connormal();
-    concrsdown(4);
-    concrsright(10);
-    conputs("Press any key to continue");
+    consetcrs(1,1);
+    int lines=8;
+    const char *text[]={
+        "This is some bogus text",
+        "to test out the appearance of my sick text editor",
+        "bleh"
+        "as asdadsjadsjoidsaiojdsajiosadioajdsaidojsdisoj",
+        "ls /",
+        "if a=b then",
+        "  load coolr driver",
+        "fi"
+    };
+    for (int i = 0; i < lines; i++) {
+        if (i > 0) {
+            conputs("\r\n");
+        }
+        conbold();
+        conputc(i+'0');
+        conputc(':');
+        connormal();
+        conputs(text[i]);
+    }
     while (congetc() != EOF);
     while (congetc() == EOF);
     conclrline();
